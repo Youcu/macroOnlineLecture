e08d041bd7075ed8c5e6c527bcda8414702a810d
Hooby <hooby11105@naver.com>
2025-05-12 00:50:23 +0900

    feat: id/pw input logic when env file is not exist

--- config/env_loader.py
+++ config/env_loader.py
@@ -1,11 +1,22 @@
-# Path: config/env_loader.py
 from dotenv import load_dotenv
 import os
+import stdiomask
 
 def load_credentials():
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # project root
     ENV_PATH = os.path.join(BASE_DIR, "src", "loginInfo.env")
-    load_dotenv(ENV_PATH)
-    ID = os.getenv("ID")
-    PW = os.getenv("PW")
+
+    ID = None
+    PW = None
+
+    if os.path.exists(ENV_PATH):
+        load_dotenv(ENV_PATH)
+        ID = os.getenv("ID")
+        PW = os.getenv("PW")
+
+    if not ID:
+        ID = input("ID를 입력하세요: ")
+    if not PW:
+        PW = stdiomask.getpass(prompt="비밀번호를 입력하세요: ", mask="*")
+
     return ID, PW
